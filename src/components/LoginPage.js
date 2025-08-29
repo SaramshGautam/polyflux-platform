e4ab9a9cef1785ee33987130af155d51dcf85f80
Saramsh Gautam <saramshgautam@gmail.com>
2025-08-29 16:13:57 -0500

    finishsignin link changed

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -43,7 +43,7 @@ const LoginPage = () => {
 
     const actionCodeSettings = {
       // url: "https://colla-board.vercel.app//finishSignIn", // Adjust for deployed domain later
-      url: "https://polyflux-platform.vercel.app/finishSignIn",
+      url: "https://polyflux-platform.vercel.app//finishSignIn",
       handleCodeInApp: true,
     };
 
