70f4ab50c58f568f5acb6d18ad38d7ebbb23d21e
Saramsh Gautam <saramshgautam@gmail.com>
2025-12-11 22:37:49 -0600

    Getting rid of logs in prod

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { FlashMessageProvider } from "./FlashMessageContext";
 
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
