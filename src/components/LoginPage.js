b1eb01386c6544416420fb9d0a959cfda0cd446d
Saramsh Gautam <saramshgautam@gmail.com>
2026-01-21 10:59:34 -0600

    chore(): login for Team F

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -95,7 +95,7 @@ const LoginPage = () => {
         role: userData.role || "participant",
         studyId: userData.studyId || "Eval3333",
         taskName: userData.taskName || "ConditionC1",
-        teamId: userData.teamId || "TeamE",
+        teamId: userData.teamId || "TeamF",
         createdAt: serverTimestamp(),
       });
 
@@ -103,7 +103,7 @@ const LoginPage = () => {
 
       const studyId = userData.studyId || "Eval3333";
       const taskName = userData.taskName || "ConditionC1";
-      const teamId = userData.teamId || "TeamE";
+      const teamId = userData.teamId || "TeamF";
 
       navigate(
         `/whiteboard/${encodeURIComponent(studyId)}/${encodeURIComponent(
